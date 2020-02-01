e545c22af207d104939f943bfda8464a11e4a049
Luism <luis.mendoza.0896@gmail.com>
2020-01-31 22:58:35 -0600

    route login-registrer-notfound

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => (
+
+  <>
+    <h1>No Encontrado, Vuelve al Inicio</h1>
+    <a href='/'>
+        Regresa al Home
+    </a>
+  </>
+
+);
+
+export default NotFound;
