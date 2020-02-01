1c4a57f16c17f5656d69212c21b6743cd50b67e3
Luism <luis.mendoza.0896@gmail.com>
2020-01-31 23:17:43 -0600

    route Link navegación

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-trailing-spaces */
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categoria from '../components/Categoria';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/InitialState';
 
 const API = 'http://localhost:3000/initalState';
@@ -14,8 +12,8 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const InitialState = useInitialState(API);
   return InitialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
-      <Header />
+    <>
+      
       <Search />
       {InitialState.mylist.length > 0 && (
         <Categoria title='Mi Lista'>
@@ -34,8 +32,8 @@ const Home = () => {
           {InitialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categoria>
-      <Footer />
-    </div>
+      
+    </>
   );
 };
 
